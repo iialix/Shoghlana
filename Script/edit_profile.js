acc39e112927a6642e65e887c75efb247b0f299e
Moaaz_Ahmed <159401570+moaaz-ahmed-kareem@users.noreply.github.com>
2025-04-25 15:38:20 +0300

    Update edit_profile.js

--- Script/edit_profile.js
+++ Script/edit_profile.js
@@ -1,6 +1,5 @@
 window.onload = function () {
     document.getElementById("name").value = localStorage.getItem("name") || "";
-    document.getElementById("email").value = localStorage.getItem("email") || "";
     document.getElementById("location").value = localStorage.getItem("location") || "";
     document.getElementById("occupation").value = localStorage.getItem("occupation") || "";
     document.getElementById("skills").value = localStorage.getItem("skills") || "";
@@ -9,14 +8,12 @@ window.onload = function () {
 
 function saveProfileChanges() {
     const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
     const location = document.getElementById("location").value;
     const occupation = document.getElementById("occupation").value;
     const skills = document.getElementById("skills").value;
     const experience = document.getElementById("experience").value;
 
     localStorage.setItem("name", name);
-    localStorage.setItem("email", email);
     localStorage.setItem("location", location);
     localStorage.setItem("occupation", occupation);
     localStorage.setItem("skills", skills);
