ddf9197f13fa2d48e506cc0c85a46c2642a7c962
Moaaz_Ahmed <159401570+moaaz-ahmed-kareem@users.noreply.github.com>
2025-04-25 16:56:15 +0300

    Update profile.js

--- Script/profile.js
+++ Script/profile.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
     document.getElementById("profile-name").textContent = localStorage.getItem("name") || "Default Name";
+    document.getElementById("profile-email").textContent = localStorage.getItem("email") || "email@example.com";
     document.getElementById("profile-location").textContent = localStorage.getItem("location") || "Unknown";
     document.getElementById("profile-occupation").textContent = localStorage.getItem("occupation") || "Occupation";
     document.getElementById("profile-skills").textContent = localStorage.getItem("skills") || "Skills";
