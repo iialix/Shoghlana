baf5c64280129d1708c3fa61f91418c158ea2cbd
moaaz ahmed <moaaz.a.kareem@gmail.com>
2025-04-26 21:33:26 +0300

    moved JavaScript code to input_validation.js

--- Script/input_validation.js
+++ Script/input_validation.js
@@ -73,3 +73,14 @@ document.getElementById('signupForm').addEventListener('submit', function (e) {
     }
 });
 
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
